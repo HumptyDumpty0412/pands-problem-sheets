c0f8e406a8569c7576feb11741791d2d447d1f26
Elena Chikanchi <124403326+HumptyDumpty0412@users.noreply.github.com>
2023-05-01 20:10:39 +0100

    Update accounts.py

--- accounts.py
+++ accounts.py
@@ -5,7 +5,5 @@
 
 # Author: Elena Chikanchi (Im write a recipe)
 
-a = int(input("Please enter an 10 digit account number: ")) #int(input) despite the 10
-x = "0123456789" # I need to show last 4 digits so 1)"replace"function  it doesnt work
-                 # "len(x)" doesnt work. Put "0"in beginning (start counting). Using "list"
-print("XXXXXX"+x[6:]) 
+a = input("Please enter an 10 digit account number: "))  #"0"in beginning (start counting). 
+print("XXXXXX"+a[6:]) 
