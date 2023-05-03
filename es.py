9899a677e1a4ff3ca38ce5d3841dd8a16f3983ad
Elena Chikanchi <124403326+HumptyDumpty0412@users.noreply.github.com>
2023-05-03 06:44:09 +0100

    Update es.py

--- es.py
+++ es.py
@@ -3,11 +3,17 @@
 
 #Author: Elena Chikanchi
 
-import sys #
+import sys #I choose sys module because it works with my VS code
+# I need sys.argv works with string (my textfile) and return argument [1] (my text)
 
-with open(sys.argv[1], encoding='utf-8') as file:
+with open(sys.argv[1], encoding='utf-8') as file: 
+    #Use encoding = "utf-8" because my .txt doesnt read 
+    
     text = file.read()
+    
     n = text.count("e")
+    # I need count all e letters in my text
+    
     print(n)
 
 
